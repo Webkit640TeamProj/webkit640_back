917fe1c47db5cde45b5d3fc485c2f6bff280ce1a
songmingyu0506 <ggp04114@naver.com>
2022-10-21 14:48:31 +0900

    Initial commit

---
+++ src/main/java/com/example/webkit640/Webkit640backApplication.java
@@ -0,0 +1,13 @@
+package com.example.webkit640;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webkit640backApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Webkit640backApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/webkit640/Webkit640backApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webkit640;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Webkit640backApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
