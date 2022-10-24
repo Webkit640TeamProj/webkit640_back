0b5874e576d2b153853eb97ba3faf58e6c2bd5eb
강승우 <kso111213@naver.com>
2022-10-24 15:18:28 +0900

    feat: add Repository, add Entity, update Entity 레파지토리 파일 추가, 생성일/수정일 생성 엔티티 추가, 어플리케이션에 어노테이션 추가, File.java -> FileEntity.java, 엔티티에 fetch 추가 -WebKit640backApplication.java -ApplicantRepository.java -AttendRepository.java -BoardRepository.java -CounselRepository.java -EmploymentRepository.java -FileRepository.java -MemberRepository.java -TraineeRepository.java -DateAudit.java -Member.java -Trainee.java -Board.java

---
+++ src/main/java/com/example/webkit640/entity/DateAudit.java
@@ -0,0 +1,20 @@
+package com.example.webkit640.entity;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class DateAudit {
+    @CreatedDate
+    private LocalDateTime createDate;
+
+    @LastModifiedDate
+    private LocalDateTime updateDate;
+}
--- src/main/java/com/example/webkit640/entity/Employment.java
+++ src/main/java/com/example/webkit640/entity/Employment.java
@@ -10,7 +10,7 @@ import javax.persistence.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-public class Employment {
+public class Employment extends DateAudit {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column
