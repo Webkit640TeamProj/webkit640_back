86b3bee824930a377098dbdb27742410df11f85e
songmingyu0506 <ggp04114@naver.com>
2022-10-31 10:24:59 +0900

    feat: modify KakaoOauth - 카카오 로그인 서비스 에러 수정

--- src/main/java/com/example/webkit640/controller/OauthUserController.java
+++ src/main/java/com/example/webkit640/controller/OauthUserController.java
@@ -11,14 +11,12 @@ import com.example.webkit640.service.MemberService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @Slf4j
 @RestController
 @RequestMapping("/auth/oauth")
