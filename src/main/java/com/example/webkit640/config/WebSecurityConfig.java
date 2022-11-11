b15bd29e9e23bf1d1e782010f7971182ac56fe57
songmingyu0506 <ggp04114@naver.com>
2022-11-11 15:39:13 +0900

    feat: modify 설정 변경

--- src/main/java/com/example/webkit640/config/WebSecurityConfig.java
+++ src/main/java/com/example/webkit640/config/WebSecurityConfig.java
@@ -44,7 +44,7 @@ public class WebSecurityConfig {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
-                .antMatchers("/*","/h2-console/**","/auth/**", "/board/download/**", "/imagePath/**").permitAll()
+                .antMatchers("/*","/h2-console/**","/auth/**", "/board/download/**", "/imagePath/**","/main/**").permitAll()
                 .anyRequest()
                 .authenticated();
         http.exceptionHandling()
